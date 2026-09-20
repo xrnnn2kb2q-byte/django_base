1e830d0667df8c53b366504b0862b7d7d0eed458
xyh <eigo.shou@icloud.com>
2026-09-20 19:31:13 +0900

    添加模型

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+'''
+    1.模型类需要继承自 models.Model
+    2.系统会自动添加一个主键 id
+    3.字段
+        字段名 = model.类型(选项)
+        
+        字段名其实就是数据表的字段名
+        字段名不要使用python、mysql等关键字
+        
+        char(M)
+        varchar(M)
+        M就是选项
+'''
+class Book(models.Model):
+    # id
+    name = models.CharField(max_length=10)
+
+class PeopleInfo(models.Model):
+    name = models.CharField(max_length=10)
+    gender = models.BooleanField()
+    # 外键的表：人物属于哪本书
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
