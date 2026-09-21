520034b03d113a0464820059a4522ffb65feb5c5
xyh <eigo.shou@icloud.com>
2026-09-21 17:00:46 +0900

    httpresponse和jsonresponse

--- bookmanager03/book/urls.py
+++ bookmanager03/book/urls.py
@@ -1,8 +1,42 @@
-from django.urls import path
+from django.urls import path, register_converter
 from book import views
+from django.urls import converters
+
+# 1. 定义转换器
+class MobileConverter:
+    # 验证数据的关键是：正则
+    regex = "1[3-9]\d{9}"
+    # 验证没有问题的数据，给视图函数
+    def to_python(self, value):
+        return int(value)
+    # 讲匹配的结果用于反向解析传值时使用（了解）
+    def to_url(self, value):
+        return str(value)
+
+register_converter(MobileConverter, 'phone')
+#2. 先注册转换器，才能在第三步中使用
+# converter 转换器类
+# type_name 转换器名字
 
 urlpatterns = [
     path('create/',views.create_book),
-    path('<city_id>/<shop_id>',views.shop),
-    path('register/',views.register)
+
+    # <转换器名字:变量名>
+    # 转换器会对变量数据进行 正则的验证
+    path('<int:city_id>/<phone:mobile>',views.shop),
+    path('register/',views.register),
+    path('json/',views.json),
+    path('method/',views.method),
+    path('response/',views.response),
 ]
+
+'''
+class IntConverter:
+    regex = "[0-9]+"
+
+    def to_python(self, value):
+        return int(value)
+
+    def to_url(self, value):
+        return str(value)
+'''
--- bookmanager03/book/views.py
+++ bookmanager03/book/views.py
@@ -14,13 +14,60 @@ def create_book(request):
 
     return HttpResponse("create")
 
-def shop(request,city_id, shop_id):
+def shop(request,city_id, mobile):
+
+    import re
+    # if not re.match('\\d{5}', shop_id):
+    #     return HttpResponse('没有此商品')
 
     query_params = request.GET
     print(query_params)
     # < QueryDict: {} >
     # QueryDict 具有字典的特性
     return HttpResponse("shop")
+
+from django.http import HttpResponse, HttpResponseNotFound, JsonResponse
+
+def response(request):
+    # response = HttpResponse("response",status=200)
+    info = {
+        'name':'itcast',
+        'address':'shunyi'
+    }
+    girl_friends = [
+        {
+            'name':'rose',
+            'address':'shunyi'
+        },
+        {
+            'name':'jack',
+            'address':'changping'
+        }
+    ]
+    # data 返回的相应数据 一般是字典类型
+    '''
+    safe = True 是表示 我们的data 是字典数据
+    JsonResponse 可以吧字典转换为json
+    
+    现在给了一个非字典数据，出了问题 我们自己负责
+    '''
+    # response = JsonResponse(data=info,safe=False)
+    # return response
+
+    import json
+    data = json.dumps(girl_friends)
+    response = HttpResponse(data)
+    return response
+
+    # 1xx
+    # 2xx
+    #       200 成功
+    # 3xx
+    # 4xx   请求有问题
+    #       404 找不到页面 路由有问题
+    #       403 禁止访问 权限问题
+    # 5xx
+    # HTTP status code must be an integer from 100 to 599
 
 #########################
 '''
@@ -30,8 +77,6 @@ def shop(request,city_id, shop_id):
     url 以 ？为分割 分为两部分
     ？前边为 请求路径
     ？后变为 查询字符串  查询字符串 类似于字典 key=value 多个数据用&连接
-    
-    
 '''
 
 def register(request):
@@ -49,4 +94,12 @@ def json(request):
     body_dict = json.loads(body.decode())
     print(body_dict)
 
+    ###########请求头############
+    print(request.META)
+
     return HttpResponse('json')
+
+def method(request):
+    print(request.method)
+    return HttpResponse("method")
+
