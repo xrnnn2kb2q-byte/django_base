fd9cc736ec51cd3569dcc4b74ce9397fa184b293
xyh <eigo.shou@icloud.com>
2026-09-21 21:27:45 +0900

    cookie和session

--- bookmanager03/book/urls.py
+++ bookmanager03/book/urls.py
@@ -28,6 +28,10 @@ urlpatterns = [
     path('json/',views.json),
     path('method/',views.method),
     path('response/',views.response),
+    path('set_cookie/',views.set_cookie),
+    path('get_cookie/',views.get_cookie),
+    path('set_session/',views.set_session),
+    path('get_session/',views.get_session),
 ]
 
 '''
--- bookmanager03/book/views.py
+++ bookmanager03/book/views.py
@@ -106,3 +106,75 @@ def method(request):
     print(request.method)
     return HttpResponse("method")
 
+######################cookie和session############################
+
+"""
+    第一次请求，携带 查询字符串
+    http://127.0.0.1:8000/set_cookie/?username=itcast&password=123
+    服务器接收到请求之后，获取username，服务器设置cookie信息，cookie信息包括username
+    浏览器接收到服务器的响应之后，应该把cookie保存起来
+    
+    第二次及其之后的请求，我们访问http://127.0.0.1:8000/ 都会携带cookie信息，服务器就可以读取cookie信息，来判断用户身份
+"""
+
+def set_cookie(request):
+    # 1. 获取查询字符串数据
+    username = request.GET.get('username')
+    password = request.GET.get('password')
+    # 2. 服务器设置cookie信息
+    # 通过响应对象.set_cookie方法
+    response = HttpResponse("set_cookie")
+    # key, value=''
+    # max_age 是一个秒数 从响应开始 计数的一个秒数
+    response.set_cookie('name',username,max_age=60*60)
+    response.set_cookie('pwd',password)
+
+    return response
+
+def get_cookie(request):
+    # 获取cookie
+    print(request.COOKIES)
+    name = request.COOKIES.get('name')
+    return HttpResponse(name)
+
+################################################################
+
+# session 是保存在服务器端  -- 数据相对安全
+# session 需要依赖于 cookie
+
+"""
+    第一次请求 http://127.0.0.1:8000/set_session/?username=itheima   我们在服务器端设置session信息
+    服务器同时会生成一个sessionid的cookie信息
+    浏览器接收到这个信息之后，会把cookie数据保存起来
+    
+    第二次及其之后的请求 都会携带这个sessionid 服务器会验证这个sessionid 验证没有问题会读取相关数据，实现业务逻辑
+"""
+
+def set_session(request):
+
+    # 1. 模拟 获取用户信息
+    username = request.GET.get('username')
+    user_id = 1
+
+    # 2. 设置session信息
+    # 假如 我们通过模型查询 查询到了用户信息
+    request.session['user_id'] = user_id
+    request.session['username'] = username
+
+    # clear 删除session里的数据，但是key有保留
+    # request.session.clear()
+    # # flush 是删除所有的数据，包括key
+    # request.session.flush()
+
+    request.session.set_expiry(3600)
+
+    return HttpResponse("set_session")
+
+def get_session(request):
+
+    user_id = request.session.get('user_id')
+    username = request.session.get('username')
+    content = "{},{}".format(user_id, username)
+
+    return HttpResponse(content)
+
